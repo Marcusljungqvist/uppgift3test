3aae77af39e056829592a78e36090100a554e6f9
Marcus Ljungqvist <marcus.ljungqvist@yh.nackademin.se>
2020-11-03 13:27:58 +0100

    Lagt till slut på programmet

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -12,36 +11,37 @@ public class Main {
         medellängd medellängd = new medellängd();
         meterkvar längd = new meterkvar();
         Vind vinden = new Vind();
+        int fråga = 0;
 
 
-            System.out.println("välkommen till golfprogrammet!");
+        System.out.println("välkommen till golfprogrammet!");
 
-            while (true) {
+        while (true) {
 
-                System.out.println("Vad heter du och vad har du för handikapp ( exempel 10,3 ) ?");
+            System.out.println("Vad heter du och vad har du för handikapp ( exempel 10,3 ) ?");
 
-                try {
-                    person.namn = scanner.nextLine();
-                    person.handikapp = scanner.nextDouble();
-                    break;
-                } catch (Exception e) {
-                    scanner.nextLine();
-                    System.out.println("ops du kan bara skriva siffor i ålder, försök igen!");
-                }
+            try {
+                person.namn = scanner.nextLine();
+                person.handikapp = scanner.nextDouble();
+                break;
+            } catch (Exception e) {
+                scanner.nextLine();
+                System.out.println("ops du kan bara skriva siffor i ålder, försök igen!");
             }
-                System.out.println("Hej " + person.namn + " du har " + person.handikapp + " i handikapp");
-            while (true) {
-                System.out.println("hur långt slår du en järn 7a i meter?");
-                try {
-                    medellängd.medel = scanner.nextInt();
-                    break;
-                } catch (Exception e) {
-                    scanner.nextLine();
-                    System.out.println("Meter kan bara skrivas i siffror, försök igen");
-                }
+        }
+        System.out.println("Hej " + person.namn + " du har " + person.handikapp + " i handikapp");
+        while (true) {
+            System.out.println("hur långt slår du en järn 7a i meter?");
+            try {
+                medellängd.medel = scanner.nextInt();
+                break;
+            } catch (Exception e) {
+                scanner.nextLine();
+                System.out.println("Meter kan bara skrivas i siffror, försök igen");
             }
+        }
 
-            System.out.println("Du har " + vinden.vind + "meter per sekund" + " i motvind vilken ger 10% mindre längd");
+        System.out.println("Du har " + vinden.vind + "meter per sekund" + " i motvind vilken ger 10% mindre längd");
 
         while (true) {
             System.out.println("Hur långt har du till hålet ?");
@@ -57,14 +57,29 @@ public class Main {
                 }
             }
             System.out.println("-----------------------------");
-            double[] längdpåjärnsjuan = uträkning.klubba(medellängd.medel);
-            double[] vind = uträkning.vind(längdpåjärnsjuan);
-            //System.out.println(Arrays.toString(vind));
+            double[] längdpåjärnsjuan = uträkning.klubba(medellängd.medel); //räkna ut längden på järnsjuan
+            double[] vind = uträkning.vind(längdpåjärnsjuan); //lägg på vinden
+            double vilkenklubba = uträkning.längduträkning(vind); // vilken klubba ska jag använda?
 
-            //System.out.println(Arrays.toString(längdpåjärnsjuan));
-            double vilkenklubba = uträkning.längduträkning(vind);
+            while (true) {
+                try {
+                    System.out.println("\nSkriv '1' för att slå igen eller '2' för att avsluta.");
+                    fråga = scanner.nextInt();
+                    if (fråga == 1 || fråga == 2)
+                        break;
+                } catch (Exception e) {
+                    scanner.next();
+                    System.out.println("Något blev fel!, försök igen.");
+                }
+            }
 
+            switch (fråga) {
+                case 1:
+                    break;
+                case 2:
+                    System.exit(0);
 
+            }
         }
     }
 }
